01c2df540e7af1cbd70cf21eee2935b93120c69a
Salem Al-Qahtani <salem.aqahtani8@gmail.com>
2026-06-24 03:14:49 +0300

    installed socket.io and created http server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,7 @@
 import "dotenv/config";
 
+import { createServer } from "http";
+import { Server } from "socket.io";
 import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
@@ -10,7 +12,7 @@ import conversationRoutes from "./routes/conversationRoutes.js";
 import messageRoutes from "./routes/messageRoutes.js";
 
 const app = express();
-
+const server = createServer(app);
 // Middleware
 app.use(cors({ origin: process.env.FRONTEND_URL }));
 app.use(express.json());
@@ -34,6 +36,6 @@ app.use((err: any, req: any, res: any, next: any) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`server is running on port : ${PORT}`);
 });
